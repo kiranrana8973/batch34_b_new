ea8cc3c173363e4d155c5f1239e86e86b2285746
Kiran Rana <kiranrana8973@gmail.com>
2025-05-19 13:14:49 +0545

    Asd

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,5 @@
 // Material app
-import 'package:batch34_b/view/flexible_expanded_view.dart';
+import 'package:batch34_b/view/card_view.dart';
 import 'package:flutter/material.dart';
 
 class App extends StatelessWidget {
@@ -7,9 +7,6 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: FlexibleExpandedView(),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: CardView());
   }
 }
---
+++ lib/view/card_view.dart
@@ -0,0 +1,22 @@
+import 'package:batch34_b/common/my_card.dart';
+import 'package:flutter/material.dart';
+
+class CardView extends StatelessWidget {
+  const CardView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              for (int i = 0; i < 10; i++)
+                MyCard(title: "Card $i", color: Colors.amber),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
